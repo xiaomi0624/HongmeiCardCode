376a5f0b927b25aaf77512c2f53aa6a3202f4fff
hongmei Zhang <zhahongm@sheridanc.on.ca>
2022-02-05 05:57:51 -0500

    second create an object

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
- * The class where we create Cards
- * @author dancye, 2019
+ * Name : Hongmei Zhang
+ * ID : 991636280
  */
 public class CardDealer 
 {
     public static void main(String[] args)
     {
+        
+        Card twoSpace = new Card("Hearts", 2);
+        
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
     }
